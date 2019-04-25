3544d4e0b37f340e97408ddbc48f9502fd8008a9
caltalys <trandtb@greenglobal.vn>
2019-04-25 15:50:26 +0700

    update config

--- src/main/java/vn/tcx/eoffice/registry/EofficeQnRegistryApplication.java
+++ src/main/java/vn/tcx/eoffice/registry/EofficeQnRegistryApplication.java
@@ -2,10 +2,12 @@ package vn.tcx.eoffice.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableConfigServer
 public class EofficeQnRegistryApplication {
 
 	public static void main(String[] args) {
